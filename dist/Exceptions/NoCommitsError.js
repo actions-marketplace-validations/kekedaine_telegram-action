fe7f30c57f9599d7c178d3f3ebd3bc8edc283142
Dai Nguyen <daidnv@gmail.com>
2022-04-08 17:35:25 +0700

    minor update

--- dist/Exceptions/NoCommitsError.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class NoCommitsError extends Error {
-}
-exports.default = NoCommitsError;
-//# sourceMappingURL=NoCommitsError.js.map
